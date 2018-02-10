220b3e4139d485a29286eb02c7c0f421910a80d5
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-10 14:31:29 +0000

    XenonHD-ify

--- xenonhd.mk
+++ xenonhd.mk
@@ -23,13 +23,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common XenonHD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_herolte
+PRODUCT_NAME := xenonhd_herolte
 PRODUCT_DEVICE := herolte
 PRODUCT_MODEL := SM-G930F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.xenonhd.maintainer=dylanneve1 \
+    ro.xenonhd.donate="https://www.paypal.me/dylanneve"
