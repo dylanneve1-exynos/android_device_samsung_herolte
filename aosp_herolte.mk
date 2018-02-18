210be971526a5eb3214e824288d009ac010b56b1
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-18 21:23:57 +0000

    Look at that, it was an mk

--- aosp_herolte.mk
+++ aosp_herolte.mk
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 
-$(call inherit-product, device/samsung/herolte/full_herolte)
+$(call inherit-product, device/samsung/herolte/full_herolte.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/herolte/device_herolte.mk)
